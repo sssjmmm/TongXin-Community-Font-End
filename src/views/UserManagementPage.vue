a77f9eb16510e39baab0d6999fbf38c904b22e27
LoveKamila <120189976+LoveKamila@users.noreply.github.com>
2023-10-06 16:17:10 +0800

    用户管理（分页用户列表，关键字搜素）

--- src/views/UserManagementPage.vue
+++ src/views/UserManagementPage.vue
@@ -2,109 +2,48 @@
     <div>
         <div class="container">
             <div class="handle-box">
-                <searchuser @search="performSearch"/>
-                <el-button type="" :icon="Refresh" @click="pullPosts">刷新</el-button>
+                <el-select v-model="query.type" placeholder="查找方式" style="flex:2">
+                    <el-option key="1" label="用户uid" value="用户uid"></el-option>
+                    <el-option key="2" label="用户名" value="用户名"></el-option>
+                </el-select>
+                <el-input v-model="query.content" style="flex: 10"></el-input>
+                <el-button type="" :icon="Search" @click="performSearch" style="flex: 1">搜索</el-button>
+                <el-button type="" :icon="Clock" @click="resetSearch" style="flex: 1;margin-left: 0;">重置</el-button>
+                <el-button type="" :icon="Refresh" @click="pullUsers" style="flex: 1;margin-left: 0;">刷新</el-button>
             </div>
+            
             <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
                 <el-table-column prop="uid" label="UID" width="100" align="center"></el-table-column>
                 <el-table-column prop="username" width="150" label="用户名" align="center"></el-table-column>
-                <!-- <el-table-column prop="status" width="130" label="状态" align='center'>
-                    <template #default="scope">
-                        <el-tag
-                            :type="scope.row.status === '通过' ? 'success' : (scope.row.status === '待审核' ? 'info' : (scope.row.status === '不通过' ? 'danger' : ''))">
-                            {{ scope.row.status }}
-                        </el-tag>
-                    </template>
-                </el-table-column> -->
-                <el-table-column prop="picture" label="图片内容" align="center">
-                    <template #default="scope">
-                        <div class="image-container">
-                            <el-image v-for="(url, index) in scope.row.picture" :key="index" class="table-td-thumb"
-                                :src="url" :z-index="10" :preview-src-list="[url]" preview-teleported></el-image>
-                        </div>
-                    </template>
-                </el-table-column>
-
                 <el-table-column prop="campus" label="学校" align="center"></el-table-column>
                 <el-table-column prop="followednum" label="粉丝数量" align="center"></el-table-column>
                 <el-table-column prop="email" label="邮箱" align="center"></el-table-column>
                 <el-table-column prop="info" label="登录信息" width="200" align="center"
                     :show-overflow-tooltip="true"></el-table-column>
-                
-                <!-- <el-table-column prop="status" label="操作" width="220" align="center">
-                    <template #default="scope">
-                        <el-button text :icon="Edit" @click="handleEdit(scope.$index, scope.row)"
-                            v-if="scope.row.status === '通过'" v-permiss="15">
-                            编辑
-                        </el-button>
-                        <el-button class="green" :icon="SuccessFilled" v-if="scope.row.status === '待审核'" text>
-                            通过
-                        </el-button>
-                        <el-button class="red" :icon="CircleCloseFilled" v-if="scope.row.status === '待审核'" text>
-                            不通过
-                        </el-button>
-
-                        <el-button class="red" :icon="CircleCloseFilled" v-if="scope.row.status === '通过'" text>
-                            驳回
-                        </el-button>
-
-                        <el-button class="green" :icon="SuccessFilled" v-if="scope.row.status === '不通过'" text>
-                            重新通过
-                        </el-button>
-                    </template>
-                </el-table-column> -->
             </el-table>
+
             <div class="pagination">
                 <!--⭐ 需要修改 -->
                 <el-pagination background layout="total, prev, pager, next" :current-page="query.pageIndex"
                     :page-size="query.pageSize" :total="pageTotal" @current-change="handlePageChange"></el-pagination>
             </div>
         </div>
-
-        <!-- 编辑弹出框 -->
-        <!-- <el-dialog title="编辑" v-model="editVisible" width="30%">
-            <el-form label-width="100px">
-                <el-form-item label="用户ID">
-                    {{ form.id }}
-                </el-form-item>
-                <el-form-item label="内容">
-                    <el-input type="textarea" rows="10" v-model="form.content"></el-input>
-                </el-form-item>
-                <el-form-item label="指定粉丝数量">
-                    <el-select v-model="followedNumber" class="handle-select mr10">
-                        <el-option key="1" label="不限" value="不限"></el-option>
-                        <el-option key="7" label="不允许关注" value="不允许关注"></el-option>
-                        <el-option key="2" label="10" value="10"></el-option>
-                        <el-option key="3" label="100" value="100"></el-option>
-                        <el-option key="4" label="500" value="500"></el-option>
-                        <el-option key="5" label="1k" value="1k"></el-option>
-                        <el-option key="6" label="5k" value="5k"></el-option>
-                    </el-select>
-                </el-form-item>
-            </el-form>
-            <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="editVisible = false">取 消</el-button>
-                    <el-button type="" @click="saveEdit">确 定</el-button>
-                </span>
-            </template>
-        </el-dialog> -->
     </div>
 </template>
 
 <script setup lang="ts" name="basetable">
 import { ref, reactive } from 'vue';
-import { ElMessage, ElMessageBox } from 'element-plus';
+import { ElMessage, ElMessageBox, dialogEmits } from 'element-plus';
 import { Delete, Edit, Search, Plus, User, Refresh, SuccessFilled, CircleCloseFilled } from '@element-plus/icons-vue';
 import { fetchTableData } from '../api/index';
 import txcRequest from '../service/index.js';
 import searchuser from '../components/searchuser.vue';
+import axios from "axios";
 
 // 表格中的项目
 interface UserItem {
     uid: number;              // 用户ID
     password: string;
-    token: string;
     picture: Array<string>;  // 图片url数组
     nickname: string;
     email: string;           // 邮箱
@@ -113,17 +52,17 @@ interface UserItem {
     phone: string;
     info: string;           // 登录信息
     followednum: number    // 被关注的数量
-    oldname: string;
     username: string;            // 用户名
 }
 
 const followedNumber = ref()
 
+
+const size = 25;
+
 const query = reactive({
-    address: '',
-    name: '',
     pageIndex: 1,
-    pageSize: 10,
+    pageSize: size,
     type: '',
     content: ''
 });
@@ -131,26 +70,25 @@ const query = reactive({
 
 const tableData = ref<Array<UserItem>>([]);
 const pageTotal = ref(0);
+const detailData = ref<UserItem>();
+
+let queryData = {
+    "uid": null,
+    "username": null
+}
+
+const restQueryData = () => {
+    queryData.uid = null;
+    queryData.username = null;
+}
 
 /**
 * searchuser组件定义的事件
 * 根据关键词查找用户
 */
-const performSearch = (query: {type: string, content: string})=>{
-    txcRequest.request({
-        url: '/user/userInfo/{username}',
-        method: 'GET',
-        params: {
-            type: query.type,
-            keyword: query.content,
-            pageSize: 1,
-            pageNum: 25,
-        }
-    }).then((res: any)=>{
-        console.log(query);
-        tableData.value =[];
-        tableData.value = res.data;
-    });
+const performSearch = () => {
+    query.pageIndex = 1;
+    pullUsers();
 }
 
 /**
@@ -159,62 +97,42 @@ const performSearch = (query: {type: string, content: string})=>{
 
 const pullUsers = () => {
     txcRequest.request({
-        url: 'user/userInfo/{username}',
-        method: 'GET',
+        url: 'manage/page/user',
+        method: 'POST',
         params: {
-            pageNum: 1,
-            pageSize: 25
+            pageNum: query.pageIndex,
+            pageSize: size
+        },
+        data: {
+            uid: query.type === "用户uid" ? query.content: null,
+            username: query.type === "用户名" ? query.content: null,
         }
     }).then((res: any)=>{
-        tableData.value = res.data;
+        if (!res.code) {
+            tableData.value = res.user_result;
+            pageTotal.value = res.totalNum;
+            console.log(pageTotal.value);
+        }
     });
 }
+
 pullUsers();
 
-// 获取表格数据
-const getData = () => {
-    fetchTableData().then(res => {
-        tableData.value = res.data.list;
-        pageTotal.value = res.data.pageTotal || 50;
-    });
-};
+
+const resetSearch = () => {
+    query.pageIndex= 1;
+    query.pageSize= size;
+    query.type= '';
+    query.content= '';
+    pullUsers();
+}
 
 // 分页导航
 const handlePageChange = (val: number) => {
     query.pageIndex = val;
-    getData();
+    pullUsers();
 };
 
-// 删除操作
-const handleDelete = (index: number) => {
-    // 二次确认删除
-    ElMessageBox.confirm('确定要删除吗？', '提示', {
-        type: 'warning'
-    })
-        .then(() => {
-            ElMessage.success('删除成功');
-            tableData.value.splice(index, 1);
-        })
-        .catch(() => { });
-};
-
-// 表格编辑时弹窗和保存
-const editVisible = ref(false);
-let form = reactive({
-    id: '',
-    content: ''
-});
-let idx: number = -1;
-const handleEdit = (index: number, row: any) => {
-    idx = index;
-    form.id = row.id;
-    form.content = row.content;
-    editVisible.value = true;
-};
-const saveEdit = () => {
-    editVisible.value = false;
-    ElMessage.success(`修改第 ${idx + 1} 行成功`);
-};
 </script>
 
 <style scoped>
@@ -281,4 +199,3 @@ const saveEdit = () => {
     max-width: 40% !important;
 }
 </style>
-
